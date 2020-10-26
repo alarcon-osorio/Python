0e6fbd82323100dcf1de66f82045755ad14b3480
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 14:54:15 -0500

    Codicionales I. Close #10

---
+++ Pildoras Python/Video10Condicionales.py
@@ -0,0 +1,14 @@
+#Valor por teclado
+print("Programa NOTAS")
+#Se debe castear un input si lo queremos numerico
+nota_alumno=input("Introduce la nota: ")
+
+#Condicional IF
+def evaluacion(nota):
+    valoracion="Aprobado"
+    if nota<5:
+        valoracion = "Reprobado"
+        calificacion = 7 #Solo accesible desde el IF - Aclaración
+    return valoracion
+
+print(evaluacion(int(nota_alumno)))
