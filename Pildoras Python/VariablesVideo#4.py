26c3a4eafd02d8d1c8246e6e5e9773733fd10a43
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 11:16:48 -0500

    Variables. Close #4

---
+++ Pildoras Python/VariablesVideo#4.py
@@ -0,0 +1,20 @@
+numero = 5
+print (type(numero))
+
+nombre="Jeison"
+print(type(nombre))
+
+decimal=5.2
+print(type(nombre))
+
+mensaje="""esto es un mensaje
+... con tres saltos
+... de linea """
+print(mensaje)
+
+numero1 = 5
+numero2 = 7
+if numero1 > numero2 :
+    print("El numero 1 es mayor")
+else:
+    print("El numero 2 es mayor")
--- Pildoras Python/Video#3-SintaxisBasica.py
+++ Pildoras Python/Video#3-SintaxisBasica.py
@@ -12,3 +12,5 @@ for i in range(5):
 	a+=1
 	print(a)
 print("hola")
+
+
