db3f8177ca2ec6464ebf35f89f9e00b88ffa9c04
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 10:56:33 -0500

    Sintaxis Basica inicio. Close #3

---
+++ Pildoras Python/Video#3-SintaxisBasica.py
@@ -0,0 +1,14 @@
+print("Hola Mundo")
+print("Hola Mundo"); print ("Adios Mundo")
+#Comentarios
+mi_nombre="Jeison"
+mi_nombre
+#Varias Lienas
+mi_nombre="mi nombre es\
+Jeison"
+mi_nombre
+a=0
+for i in range(5):
+	a+=1
+	print(a)
+print("hola")
