20e701c98089f58b8c56e187082c3358e79f463c
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 11:27:29 -0500

    Funciones. Close #5

---
+++ Pildoras Python/Video5Funciones.py
@@ -0,0 +1,10 @@
+#Declaracion de funciones
+def mensaje():
+    #Cuerpo de Función
+    print("Aprendiendo Python")
+    print("Aprendiendo Python")
+    print("Aprendiendo Python")
+
+#Llamado de función 
+mensaje()
+mensaje()
