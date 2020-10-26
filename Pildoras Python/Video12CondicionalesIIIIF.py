11eea09a8f6c71408465ad66862d17ab0163ac30
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-26 15:21:50 -0500

    Condicionales III IF. Close #12

---
+++ Pildoras Python/Video12CondicionalesIIIIF.py
@@ -0,0 +1,8 @@
+edad = -7
+#Concatenacion de operadores de comparacion
+if 0 < edad < 100: #Evalua las dos condiciones en caso de que sea menor a 0, inmediatamenta la salta
+    print("Edad Correcta")
+else:
+    print("Edad incorrecta")
+
+#
