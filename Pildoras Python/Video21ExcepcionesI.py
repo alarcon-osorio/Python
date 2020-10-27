2dd41e37d8b6dbeaad62f860e32c1c8ba98abaec
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-27 14:58:32 -0500

    Excepciones I. Close #21

---
+++ Pildoras Python/Video21ExcepcionesI.py
@@ -0,0 +1,44 @@
+def suma(num1, num2):
+	return num1+num2
+
+def resta(num1, num2):
+	return num1-num2
+
+def multiplica(num1, num2):
+	return num1*num2
+
+def divide(num1,num2):	
+
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+    
+    if divide == "None":
+        print("Hola")
+    
+	
+
+op1=(int(input("Introduce el primer numero: ")))
+
+op2=(int(input("Introduce el segundo numero: ")))		
+	
+operacion=input("Introduce la operacion a realizar (suma,resta,multiplica,divide): ")
+
+if operacion=="suma":
+	print(suma(op1,op2))
+
+elif operacion=="resta":
+	print(resta(op1,op2))
+
+elif operacion=="multiplica":
+	print(multiplica(op1,op2))
+
+elif operacion=="divide":
+	print(divide(op1,op2))
+
+else:
+	print ("Operacion no contemplada")
+
+
+print("Operacion ejecutada. Continuacion de ejecucion del programa ")
