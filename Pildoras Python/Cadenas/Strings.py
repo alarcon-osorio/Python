facdf720373496b227fd04a5742126546018a9cc
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-30 22:35:43 -0500

    Cadenas. Close #33

---
+++ Pildoras Python/Cadenas/Strings.py
@@ -0,0 +1,7 @@
+usuario=input("Introduce tu nombre de Usuario: ")
+print("El nombre de usuario es: ", usuario.upper()) #MAYUS
+print("El nombre de usuario es: ", usuario.lower()) #MINUS
+print("El nombre de usuario es: ", usuario.capitalize()) #PRIMER MAYUS
+
+usuario=input("Introduce tu Edad: ")
+print("El nombre de usuario es: ", usuario.isdigit()) #BOOLEAN
