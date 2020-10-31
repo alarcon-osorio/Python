3eb7cdaa6b2b2d4ec0832b858fa6b05e23984c43
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-10-30 23:47:23 -0500

    Paquetes distribuibles. Close #36

---
+++ Pildoras Python/Cadenas/IndexModulos.py
@@ -0,0 +1,5 @@
+#Video 35 - Paquetes
+from Calculos.CalculosGenerales import *
+from Calculos.Basicos.OperacionesBasicas import *
+multiplicar(2,2) #de paquete CalculosGenerales
+sumar(5,6) #de subpaquete OperacionesBasicas
---
+++ Pildoras Python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Paquete Calculos",
+    version="1.0",
+    description="Paquete de Calculos",
+    author="Jeison",
+    author_email="alarcon_osorio@hotmail.com",
+    url="www.engineersystems.com",
+    packages=["Calculos", "Calculos.Basicos"]
+)
